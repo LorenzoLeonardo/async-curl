ba7d35019f3ae032b344280097cb442e552ada1d
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-03-24 22:51:03 +0800

    Add documentation on how to use this crate

--- src/async_curl.rs
+++ src/async_curl.rs
@@ -2,6 +2,28 @@ use curl::easy::{Easy2, Handler};
 use curl::multi::Multi;
 use curl::MultiError;
 
+/// AsyncCurl is responsible for performing
+/// the contructed Easy2 object by passing
+/// it into send_request
+/// ```
+/// use curl::easy::Easy2;
+/// use async_curl::response_handler::ResponseHandler;
+/// use async_curl::async_curl::AsyncCurl;
+///
+/// # #[tokio::main(flavor = "current_thread")]
+/// # async fn main() -> Result<(), Box<dyn std::error::Error>>{
+/// let curl = AsyncCurl::new();
+/// let mut easy2 = Easy2::new(ResponseHandler::new());
+///
+/// easy2.url("https://www.rust-lang.org").unwrap();
+/// easy2.get(true).unwrap();
+///
+/// let response = curl.send_request(easy2).await.unwrap();
+/// eprintln!("{:?}", response.get_ref());
+///
+/// Ok(())
+/// # }
+/// ```
 pub struct AsyncCurl;
 
 impl AsyncCurl {
