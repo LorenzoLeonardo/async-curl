41bee1ba07eae7394e1d8ec8532e32ebea9af395
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-03-24 22:51:03 +0800

    Add documentation on how to use this crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! async-curl: An asynchronous implementation to perform curl operations with tokio.
 //! This requires a dependency with the [curl](https://crates.io/crates/curl) and [tokio](https://crates.io/crates/tokio) crates
 //!
-mod async_curl;
-mod response_handler;
+pub mod async_curl;
+pub mod response_handler;
--- src/response_handler.rs
+++ src/response_handler.rs
@@ -2,6 +2,17 @@ use curl::easy::Handler;
 use curl::easy::WriteError;
 use std::fmt::Debug;
 
+/// A handler of Easy2
+/// ```
+/// use curl::easy::Easy2;
+/// use async_curl::response_handler::ResponseHandler;
+///
+/// # fn main() {
+/// let easy2 = Easy2::new(ResponseHandler::new());
+///
+/// println!("{:?}", easy2.get_ref());
+/// # }
+/// ```
 #[derive(Debug, Clone, Default)]
 pub struct ResponseHandler {
     data: Vec<u8>,
