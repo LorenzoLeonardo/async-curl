f71313fe5fdd1fd1f0566b16a1b08ddb2339c1ae
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2023-03-25 22:34:02 +0800

    Add error handling
    Consolidated 3rd party errors to
    our error to make it as one

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,5 @@
 //! This requires a dependency with the [curl](https://crates.io/crates/curl) and [tokio](https://crates.io/crates/tokio) crates
 //!
 pub mod async_curl;
+pub mod async_curl_error;
 pub mod response_handler;
